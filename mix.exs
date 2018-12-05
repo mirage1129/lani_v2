68ec785bd026234f7f00270aeadf79916285a7fc
mirage1129 <meraj.a.qazi@gmail.com>
2018-12-05 14:10:02 +0800

    removed Phoenix from homepage

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lani.MixProject do
   def project do
     [
       app: :lani,
-      version: "0.1.0",
+      version: "0.0.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
