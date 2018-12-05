7c64bc55436077396f633c164dbbd3709301d41b
mirage1129 <meraj.a.qazi@gmail.com>
2018-12-05 20:00:30 +0800

    bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lani.MixProject do
   def project do
     [
       app: :lani,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
